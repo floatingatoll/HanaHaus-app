607654491a6a6fb07ab0c67abf51250e2cfced8d
Gabriel Ruiz <gabe@ggruiz.me>
2015-02-16 11:41:10 -0800

    first attempts at integrating bower

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,15 @@
+var $ = jquery;
+$(document).ready(function() {
+  $('#about-footer').click(function() {
+    console.log('about');
+  });
+  $('#partners-footer').click(function() {
+    console.log('partners');
+  });
+  $('#social-footer').click(function() {
+    console.log('social');
+  });
+  $('#find-footer').click(function() {
+    console.log('find');
+  });
+});
