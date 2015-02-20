61f52c30775b54d4685402f85621ce026097d28b
Gabriel Ruiz <gabe@ggruiz.me>
2015-02-20 11:49:37 -0800

    social media css janky fix

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -5,13 +5,21 @@ $(document).ready(function() {
   $('#about-footer').click(function() {
     console.log('about');
   });
+
   $('#partners-footer').click(function() {
     console.log('partners');
   });
+
   $('#social-footer').click(function() {
     console.log('social');
   });
+
   $('#find-footer').click(function() {
     console.log('find');
+  });
+
+  $('.socialmedia .image').css({
+    'background-repeat': 'no-repeat',
+    'background-position': 'right'
   });
 });
